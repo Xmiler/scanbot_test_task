874d7270fa9787942cb0510e717124fe9b8b2d53
Alexander Uldin <alex.uldin@gmail.com>
2019-11-12 22:48:05 +0300

    fix dataset module :)

--- dataset/synthetic_card_image_dataset.py
+++ dataset/synthetic_card_image_dataset.py
@@ -2,13 +2,17 @@ from numpy.random import randint
 from PIL import Image
 
 from torch.utils.data import Dataset
+from torchvision import transforms
 
-from synthetic_card_image_generator import SyntheticCardImageGenerator, generate_random_text, generate_random_face
+from synthetic_card_image_generator import SyntheticCardImageGenerator, generate_random_text_image, generate_random_face_image
 
 
 class SyntheticCardImageDataset(Dataset):
-    def __init__(self, transform=None):
-        self.transform = transform
+    SIZE = 256
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    def __init__(self, to_tensor=False):
+        self._to_tensor = to_tensor
 
     def __len__(self):
         return 1000  # fake size of epoch
@@ -17,15 +21,23 @@ class SyntheticCardImageDataset(Dataset):
         card_image = SyntheticCardImageGenerator()
 
         for _ in range(25):
-            text_image = generate_random_text(randint(5, 20))
+            text_image = generate_random_text_image(randint(5, 20))
             card_image.put_fragment(text_image)
 
-        face_image = generate_random_face(randint(150, 350))
+        face_image = generate_random_face_image(randint(150, 350))
 
         card_image.put_fragment(face_image)
 
         card_image.draw_pattern_texts()
 
-        card_image.apply_square_and_zooming()
+        card_image.apply_square_and_zooming(factor=1.)
 
-        return Image.fromarray(card_image.image), Image.fromarray(card_image.image_gt)
+        card_image.resize(self.SIZE)
+
+        image, image_gt = Image.fromarray(card_image.image), Image.fromarray(card_image.image_gt)
+
+        if self._to_tensor:
+            tnsf = transforms.ToTensor()
+            image, image_gt = tnsf(image), tnsf(image_gt)
+
+        return image, image_gt
--- dataset/synthetic_card_image_generator.py
+++ dataset/synthetic_card_image_generator.py
@@ -1,19 +1,17 @@
-import io
 import string
 from pathlib import Path
 
 import numpy as np
-from numpy.random import randint
+from numpy.random import randint, choice
 
 import cv2
-import PIL.Image
+from PIL import Image, ImageFont, ImageDraw
 
-from trdg.computer_text_generator import generate
 from trdg.utils import load_fonts
 
 np.random.seed(0)
 
-CHARACTERS = list(string.ascii_letters + string.digits * 3 + ':/' * 2)
+CHARACTERS = list(string.ascii_letters + string.digits * 3 + ':/'*2)
 FONTS = [font for i, font in enumerate(load_fonts('en')) if
          i in [1, 2, 15, 19, 22, 23, 24, 27, 35, 49]]  # picked MRZ like fonts
 MAGES4AUGMENT_ROOT_PATH = Path(__file__).parent / '../images4augment/'
@@ -22,20 +20,34 @@ image_face_paths = sorted([path for path in (MAGES4AUGMENT_ROOT_PATH / 'faces').
 
 
 def create_image_with_text(text, font_id=None, font_size=None):
+
     if font_id is None:
         font_id = randint(0, high=len(FONTS))
     if font_size is None:
         font_size = randint(16, high=64)
-    img = generate(text, font=FONTS[font_id], text_color='#000000', font_size=font_size, space_width=1., fit=False, orientation=0)
-    return cv2.bitwise_not(np.array(img)[:, :, 3])  # library "trdg" writes result to alpha channel. Convert to grey.
+
+    image_font = ImageFont.truetype(font=FONTS[font_id], size=font_size)
+
+    text_img = Image.new("L",
+                         image_font.getsize(text),
+                         255)
+    text_draw = ImageDraw.Draw(text_img)
+
+    text_draw.text((0, 0), text, font=image_font, fill=0)
+
+    return np.array(text_img)
 
 
-def generate_random_text(text_len, font_id=None, font_size=None):
-    text = ''.join(np.random.choice(CHARACTERS, size=text_len))
+def generate_random_text(size):
+    return ''.join(np.random.choice(CHARACTERS, size=size))
+
+
+def generate_random_text_image(text_len, font_id=None, font_size=None):
+    text = generate_random_text(text_len)
     return create_image_with_text(text, font_id=font_id, font_size=font_size)
 
 
-def generate_random_face(size):
+def generate_random_face_image(size):
     image_face_path = np.random.choice(image_face_paths, 1)[0]
     image_face = cv2.imread(image_face_path.as_posix(), cv2.IMREAD_GRAYSCALE)
     h_, w_ = image_face.shape
@@ -46,6 +58,12 @@ def generate_random_face(size):
     return image_face
 
 
+def put_fragment(image, fragment, pt):
+    size = (min(image.shape[0] - pt[0], fragment.shape[0]),
+            min(image.shape[1] - pt[1], fragment.shape[1]))
+    image[pt[0]:pt[0] + size[0], pt[1]:pt[1] + size[1]] = fragment[:size[0], :size[1]]
+
+
 class SyntheticCardImageGenerator:
     CARD_SIZE = (650, 800)
     PATTERN_TEXTS_NUM = 3
@@ -54,16 +72,21 @@ class SyntheticCardImageGenerator:
 
         self._image = 255 * np.ones(self.CARD_SIZE, dtype=np.uint8)
 
-        # generate bare pattern texts
-        self._pattern_texts = [self.generate_pattern_text_bare() for _ in range(self.PATTERN_TEXTS_NUM)]
+        # generate pattern texts
+        self._pattern_texts = ['<'*randint(1, high=20) for _ in range(self.PATTERN_TEXTS_NUM)]
 
-        # set patternt texts drawing parameters
-        pt_xs = randint(0, high=100) * np.ones((self.PATTERN_TEXTS_NUM), dtype=np.int)
-        pt_ys = self.CARD_SIZE[0] - np.cumsum(randint(50, high=100, size=(self.PATTERN_TEXTS_NUM)))
+        # set pattern texts drawing parameters
+        pt_ys = self.CARD_SIZE[0] - np.cumsum(randint(50, high=100, size=self.PATTERN_TEXTS_NUM))
         pt_ys -= 10
-        self._pattern_text_imgs_pt = list(zip(pt_ys, pt_xs))
+
         font_id = randint(0, high=len(FONTS))
-        font_size = 60
+        font_size = randint(50, 65)
+        image_font = ImageFont.truetype(font=FONTS[font_id], size=font_size)
+        ch_w = image_font.getsize('<')[0]
+
+        pt_xs = [randint(0, high=self.CARD_SIZE[1] - len(pattern_text)*ch_w) for pattern_text in self._pattern_texts]
+
+        self._pattern_text_imgs_pt = list(zip(pt_ys, pt_xs))
 
         # draw bare pattern texts
         self._pattern_text_imgs = [create_image_with_text(pattern_text, font_id=font_id, font_size=font_size) for
@@ -71,15 +94,7 @@ class SyntheticCardImageGenerator:
         self.draw_pattern_texts()
 
         # store ground truth
-        self._image_gt = cv2.bitwise_not(self._image)
-
-        # redraw with non-bare pattern text
-        self._pattern_texts = [
-            ''.join([ch if ch == '<' else np.random.choice(list(string.ascii_uppercase), 1)[0] for ch in pattern_text]) for
-            pattern_text in self._pattern_texts]
-        self._pattern_text_imgs = [create_image_with_text(pattern_text, font_id=font_id, font_size=font_size) for
-                                   pattern_text in self._pattern_texts]
-        self.draw_pattern_texts()
+        _, self._image_gt = cv2.threshold(self._image, 127, 255, cv2.THRESH_BINARY_INV)
 
     @property
     def image(self):
@@ -92,9 +107,8 @@ class SyntheticCardImageGenerator:
     def put_fragment(self, fragment, pt=None):
         if pt is None:
             pt = tuple([randint(0, high=self.CARD_SIZE[i]) for i in [0, 1]])
-        size = (min(self._image.shape[0] - pt[0], fragment.shape[0]),
-                min(self._image.shape[1] - pt[1], fragment.shape[1]))
-        self._image[pt[0]:pt[0] + size[0], pt[1]:pt[1] + size[1]] = fragment[:size[0], :size[1]]
+
+        put_fragment(self._image, fragment, pt)
 
     def draw_pattern_texts(self):
         for img_i in range(self.PATTERN_TEXTS_NUM):
@@ -114,15 +128,8 @@ class SyntheticCardImageGenerator:
         self._image = new_image
         self._image_gt = new_image_gt
 
-    @staticmethod
-    def generate_pattern_text_bare(max_size=16, pieces_num=10, piece_max_len=20):
-        while True:
-            rnd_flags = randint(0, high=2, size=pieces_num)
-            rnd_lens = randint(1, high=piece_max_len + 1, size=(pieces_num))
+    def resize(self, size):
+        self._image = cv2.resize(self._image, tuple([size]*2))
+        self._image_gt = cv2.resize(self._image_gt, tuple([size] * 2))
+        _, self._image_gt = cv2.threshold(self._image_gt, 64, 255, cv2.THRESH_BINARY)
 
-            pieces = [(' ' if f else '<') * n for f, n in zip(rnd_flags, rnd_lens)]
-            pattern_text_bare = ''.join(pieces)[:max_size]
-
-            if '<' in pattern_text_bare:
-                break
-        return pattern_text_bare
